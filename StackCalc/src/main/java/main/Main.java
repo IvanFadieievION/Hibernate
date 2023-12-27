e38b03e77b7e20212a41f0f3ff9f503fbc9b08ff
ivanfadieiev <ivanfadieiev@gmail.com>
2023-12-27 10:48:50 +0200

    removed test logic for main method

--- StackCalc/src/main/java/main/Main.java
+++ StackCalc/src/main/java/main/Main.java
@@ -2,7 +2,5 @@ package main;
 
 public class Main {
     public static void main(String[] args) {
-        Calculator calculator = new Calculator();
-        System.out.println(calculator.compute("-7"));
     }
 }
