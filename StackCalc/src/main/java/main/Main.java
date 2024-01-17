335570f48f35cb00b70c889ce3350db479398389
IvanFadieievION <155053745+IvanFadieievION@users.noreply.github.com>
2024-01-17 14:26:53 +0200

    Delete StackCalc directory

--- StackCalc/src/main/java/main/Main.java
+++
@@ -1,8 +0,0 @@
-package main;
-
-public class Main {
-    public static void main(String[] args) {
-        Calculator calculator = new Calculator();
-        System.out.println(calculator.compute("1+2"));
-    }
-}
